5cc755781b6cbdd8fead12157948fdeb300d3d2c
luocz <luoczyy@gmail.com>
2016-02-20 23:43:56 +0800

    luocz修改

--- easyshop/src/com/easyshop/core/modules/FunctionsModule.java
+++ easyshop/src/com/easyshop/core/modules/FunctionsModule.java
@@ -4,53 +4,91 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.nutz.dao.Cnd;
 import org.nutz.dao.Dao;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.By;
 import org.nutz.mvc.annotation.Fail;
+import org.nutz.mvc.annotation.Filters;
 import org.nutz.mvc.annotation.Ok;
 import org.nutz.mvc.annotation.Param;
 
-import com.easyshop.bean.Catalogs;
+import com.easy.core.filters.CheckBackUserLoginFilter;
 import com.easyshop.bean.Functions;
+import com.easyshop.core.modules.admin.OrderConstant;
 
 @IocBean
 @At("/functions")
 @Ok("json")
 @Fail("http:500")
+@Filters({ @By(type = CheckBackUserLoginFilter.class) })
 public class FunctionsModule {
 	@Inject
-    protected Dao dao;
-	
-	private Map<String,Object> result;
-	
+	protected Dao dao;
+
+	private Map<String, Object> result;
+
+	/**
+	 * 获取登录用户菜单
+	 * 
+	 * @return
+	 */
 	@At
-	public Object getAllFunctions(@Param("id")int ID){
-		this.result=new HashMap<String,Object>();
-		List<Functions> catalogs=this.query(ID);
-		this.result.put("total",catalogs.size());
-		this.result.put("rows",catalogs);
+	public Object getUserFunctions(HttpSession session) {
+
+		String userId = String.valueOf(session
+				.getAttribute(OrderConstant.FRONT_USER_ID));
+
+		List<Functions> list = dao
+				.query(Functions.class,
+						Cnd.wrap(" url is not null and pid !=0 "
+								+ "	and id in (select f.functionId from connectorFR f, user u "
+								+ " where f.roleId = u.roleId and u.userId = '"
+								+ userId + "')"));
+		return list;
+	}
+
+	@At
+	public Object getAllFunctions(@Param("id") int ID) {
+		this.result = new HashMap<String, Object>();
+		List<Functions> catalogs = this.query(ID);
+		this.result.put("total", catalogs.size());
+		this.result.put("rows", catalogs);
 		return this.result;
 	}
 
-	private List<Functions> query(int ID){
-		//第一次查询，获得所有的顶级对象
-		List<Functions> results=dao.query(Functions.class, Cnd.where("id","=",ID).asc("orderId"));
-		//第二次查询。获取子级对象
-		for(Functions catalog : results){
+	@At
+	public Object queryAllFunctions() {
+		this.result = new HashMap<String, Object>();
+		List<Functions> functions = dao.query(Functions.class, null);
+		this.result.put("total", functions.size());
+		this.result.put("rows", functions);
+		return this.result;
+	}
+
+	private List<Functions> query(int ID) {
+		// 第一次查询，获得所有的顶级对象
+		List<Functions> results = dao.query(Functions.class,
+				Cnd.where("id", "=", ID).asc("orderId"));
+		// 第二次查询。获取子级对象
+		for (Functions catalog : results) {
 			this.subquery(catalog, catalog.getId());
 		}
 		return results;
 	}
-	private void subquery(Functions catalog,int ID){
-		List<Functions> temp = dao.query(Functions.class, Cnd.where("pid","=",catalog.getId()).asc("orderId"));
-//		if(temp.size()>0){
-			catalog.setSubFunctions(temp);
-//		}
-		for(Functions subtemp :temp){
-			subquery(subtemp,subtemp.getId());
+
+	private void subquery(Functions catalog, int ID) {
+		List<Functions> temp = dao.query(Functions.class,
+				Cnd.where("pid", "=", catalog.getId()).asc("orderId"));
+		// if(temp.size()>0){
+		catalog.setSubFunctions(temp);
+		// }
+		for (Functions subtemp : temp) {
+			subquery(subtemp, subtemp.getId());
 		}
 		return;
 	}
--- easyshop/src/com/easyshop/core/modules/RoleModule.java
+++ easyshop/src/com/easyshop/core/modules/RoleModule.java
@@ -26,72 +26,84 @@ import com.easyshop.bean.User;
 public class RoleModule {
 
 	@Inject
-    protected Dao dao;
-	
-	private Map<String,Object> result;
-	
+	protected Dao dao;
+
+	private Map<String, Object> result;
+
 	@At
-	public Object getRoleLists(){
-		this.result=new HashMap<String,Object>();
+	public Object getRoleLists() {
+		this.result = new HashMap<String, Object>();
 		List<Role> roles = dao.query(Role.class, null);
-		
-		for(Role role : roles){
-		    role.setFunctions(dao.query(Functions.class, Cnd.wrap(" id in (select functionId from connectorFR t where t.roleId ="+role.getRoleId()+" )")));
+		for (Role role : roles) {
+			role.setFunctions(dao.query(
+					Functions.class,
+					Cnd.wrap(" id in (select functionId from connectorFR t where t.roleId ="
+							+ role.getRoleId() + " )")));
 		}
-		
 		this.result.put("total", roles.size());
 		this.result.put("rows", roles);
 		return this.result;
 	}
+
 	@At
-	public Object addRole(@Param("rolename")String name,@Param("roleDescp")String descp){
-		this.result=new HashMap<String,Object>();
-		if(name.equals("superAdmin")){
+	public Object getRoleIdNames() {
+		List<Role> roles = dao.query(Role.class, null);
+		return roles;
+	}
+
+	@At
+	public Object addRole(@Param("rolename") String name,
+			@Param("roleDescp") String descp) {
+		this.result = new HashMap<String, Object>();
+		if (name.equals("superAdmin")) {
 			this.result.put("status", "fail");
-			this.result.put("msg","与超极管理员重名！");
+			this.result.put("msg", "与超极管理员重名！");
 			return this.result;
 		}
 		Role role = new Role();
 		role.setName(name);
 		role.setDescp(descp);
-		//进行插入操作
-		if(dao.insert(role)!=null){
-			//插入成功
-			this.result.put("status","success");
+		// 进行插入操作
+		if (dao.insert(role) != null) {
+			// 插入成功
+			this.result.put("status", "success");
 			return this.result;
-		}else{
-			//插入失败
+		} else {
+			// 插入失败
 			this.result.put("status", "fail");
 			this.result.put("msg", "操作失败");
 			return this.result;
 		}
 	}
+
 	@At
-	public Object deleteOne(@Param("roleId")int ID){
-		//在这里需要注意的是一旦角色被删除，与其相关联的用户也要进行关联更新
-		this.result=new HashMap<String,Object>();
-		List<User> users=dao.query(User.class,Cnd.where("roleId","=",ID));
-		for(User temp : users){
+	public Object deleteOne(@Param("roleId") int ID) {
+		// 在这里需要注意的是一旦角色被删除，与其相关联的用户也要进行关联更新
+		this.result = new HashMap<String, Object>();
+		List<User> users = dao.query(User.class, Cnd.where("roleId", "=", ID));
+		for (User temp : users) {
 			temp.setRoleId(0);
 			dao.update(temp);
 		}
-		if(dao.delete(Role.class,ID)>0){
+		if (dao.delete(Role.class, ID) > 0) {
 			this.result.put("status", "success");
-		}else{
+		} else {
 			this.result.put("status", "fail");
 			this.result.put("msg", "删除失败");
 		}
 		return this.result;
 	}
+
 	@At
-	public Object updateRole(@Param("roleId")int ID,@Param("rolename")String name,@Param("roleDescp")String descp){
-		this.result=new HashMap<String,Object>();
-		Role role=dao.fetch(Role.class,Cnd.where("roleId","=",ID));
+	public Object updateRole(@Param("roleId") int ID,
+			@Param("rolename") String name, @Param("roleDescp") String descp) {
+		this.result = new HashMap<String, Object>();
+		Role role = dao.fetch(Role.class, Cnd.where("roleId", "=", ID));
 		role.setName(name);
 		role.setDescp(descp);
-		if(dao.update(role)>0){
-			this.result.put("status","success");
-		}else{
+		if (dao.update(role) > 0) {
+			this.result.put("status", "success");
+		} else {
 			this.result.put("status", "fail");
 			this.result.put("msg", "更新失败");
 		}
@@ -99,15 +111,17 @@ public class RoleModule {
 	}
 
 	@At
-	public Object addRolePrivilege(@Param("roleId")int roleId,@Param("privilegeIds")String privilegeIds){
-		this.result=new HashMap<String,Object>();
-		String[] priIds=privilegeIds.split("@");
-		//在插入之前先删除
-		dao.clear(ConnectorFR.class,Cnd.where("roleId","=",roleId));
-		Role role= dao.fetch(Role.class,Cnd.where("roleId","=",roleId));
-		List<Functions> funs=dao.query(Functions.class, Cnd.where("id","in",priIds));
-		for(String id : priIds){
-			ConnectorFR cfr=new ConnectorFR();
+	public Object addRolePrivilege(@Param("roleId") int roleId,
+			@Param("privilegeIds") String privilegeIds) {
+		this.result = new HashMap<String, Object>();
+		String[] priIds = privilegeIds.split("@");
+		// 在插入之前先删除
+		dao.clear(ConnectorFR.class, Cnd.where("roleId", "=", roleId));
+		// Role role= dao.fetch(Role.class,Cnd.where("roleId","=",roleId));
+		// List<Functions> funs=dao.query(Functions.class,
+		// Cnd.where("id","in",priIds));
+		for (String id : priIds) {
+			ConnectorFR cfr = new ConnectorFR();
 			cfr.setRoleId(roleId);
 			cfr.setFunctionId(Integer.parseInt(id));
 			dao.insert(cfr);
@@ -115,61 +129,70 @@ public class RoleModule {
 		this.result.put("status", "success");
 		return this.result;
 	}
-	
+
 	@At
-	public Object getRolePrivileges(@Param("userName")String userName){
-		this.result=new HashMap<String,Object>();
-		User user = dao.fetch(User.class,Cnd.where("name","=",userName));
-		Role role = dao.fetch(Role.class,Cnd.where("roleId","=",user.getRoleId()));
-		List<ConnectorFR> conns= dao.query(ConnectorFR.class,Cnd.where("roleId","=",user.getRoleId()));
-		String[] priIds=new String[conns.size()];
+	public Object getRolePrivileges(@Param("userName") String userName) {
+		this.result = new HashMap<String, Object>();
+		User user = dao.fetch(User.class, Cnd.where("name", "=", userName));
+		Role role = dao.fetch(Role.class,
+				Cnd.where("roleId", "=", user.getRoleId()));
+		List<ConnectorFR> conns = dao.query(ConnectorFR.class,
+				Cnd.where("roleId", "=", user.getRoleId()));
+		String[] priIds = new String[conns.size()];
 		int i = 0;
-		for(ConnectorFR conn : conns){
-			priIds[i]=String.valueOf(conn.getFunctionId());
+		for (ConnectorFR conn : conns) {
+			priIds[i] = String.valueOf(conn.getFunctionId());
 			i++;
 		}
-		List<Functions> funs = dao.query(Functions.class, Cnd.where("id","in",priIds).asc("orderId"));
-		List<Functions> results= new ArrayList<Functions>();
-		for(Functions fs : funs){
+		List<Functions> funs = dao.query(Functions.class,
+				Cnd.where("id", "in", priIds).asc("orderId"));
+		List<Functions> results = new ArrayList<Functions>();
+		for (Functions fs : funs) {
 			List<Functions> fun = this.query(fs.getId());
 			results.add(fun.get(0));
 		}
-		this.result.put("rows",results);
+		this.result.put("rows", results);
 		return this.result;
 	}
-	
+
 	@At
-	public Object getCurrentFuns(@Param("roleId")int ID){
-		this.result=new HashMap<String,Object>();
-		List<ConnectorFR> conns= dao.query(ConnectorFR.class,Cnd.where("roleId","=",ID));
-		if(conns.size()>0){
-			String[] priIds=new String[conns.size()];
+	public Object getCurrentFuns(@Param("roleId") int ID) {
+		this.result = new HashMap<String, Object>();
+		List<ConnectorFR> conns = dao.query(ConnectorFR.class,
+				Cnd.where("roleId", "=", ID));
+		if (conns.size() > 0) {
+			String[] priIds = new String[conns.size()];
 			int i = 0;
-			for(ConnectorFR conn : conns){
-				priIds[i]=String.valueOf(conn.getFunctionId());
+			for (ConnectorFR conn : conns) {
+				priIds[i] = String.valueOf(conn.getFunctionId());
 				i++;
 			}
-			List<Functions> funs = dao.query(Functions.class, Cnd.where("id","in",priIds));
+			List<Functions> funs = dao.query(Functions.class,
+					Cnd.where("id", "in", priIds));
 			this.result.put("rows", funs);
 		}
 		return this.result;
 	}
-	private List<Functions> query(int ID){
-		//第一次查询，获得所有的顶级对象
-		List<Functions> results=dao.query(Functions.class, Cnd.where("id","=",ID).asc("orderId"));
-		//第二次查询。获取子级对象
-		for(Functions catalog : results){
+
+	private List<Functions> query(int ID) {
+		// 第一次查询，获得所有的顶级对象
+		List<Functions> results = dao.query(Functions.class,
+				Cnd.where("id", "=", ID).asc("orderId"));
+		// 第二次查询。获取子级对象
+		for (Functions catalog : results) {
 			this.subquery(catalog, catalog.getId());
 		}
 		return results;
 	}
-	private void subquery(Functions catalog,int ID){
-		List<Functions> temp = dao.query(Functions.class, Cnd.where("pid","=",catalog.getId()).asc("orderId"));
-//		if(temp.size()>0){
-			catalog.setSubFunctions(temp);
-//		}
-		for(Functions subtemp :temp){
-			subquery(subtemp,subtemp.getId());
+
+	private void subquery(Functions catalog, int ID) {
+		List<Functions> temp = dao.query(Functions.class,
+				Cnd.where("pid", "=", catalog.getId()).asc("orderId"));
+		// if(temp.size()>0){
+		catalog.setSubFunctions(temp);
+		// }
+		for (Functions subtemp : temp) {
+			subquery(subtemp, subtemp.getId());
 		}
 		return;
 	}
